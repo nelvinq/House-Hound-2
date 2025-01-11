8fc70f37ea45bcf3f8e6cf1a5a9a243f377bb2b6
Nelvin <nelvinq@gmail.com>
2025-01-11 11:12:22 +0800

    chore: update API

--- src/services/propertyData.js
+++ src/services/propertyData.js
@@ -1,7 +1,6 @@
 const PROPERTY_TABLE_URL = `${import.meta.env.VITE_PROPERTY_TABLE_URL}`
 const AIRTABLE_ACCESS_TOKEN = `${import.meta.env.VITE_AIRTABLE_ACCESS_TOKEN}`
 
-
 const propertyData = async () => {
   try {
     const res = await fetch(PROPERTY_TABLE_URL, {headers: {
