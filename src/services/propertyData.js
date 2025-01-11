8964f50fc6b10257226b5d8edff2816920d13559
Nelvin <nelvinq@gmail.com>
2025-01-11 10:57:35 +0800

    feat: update API

--- src/services/propertyData.js
+++ src/services/propertyData.js
@@ -1,9 +1,10 @@
 const PROPERTY_TABLE_URL = `${import.meta.env.VITE_PROPERTY_TABLE_URL}`
 const AIRTABLE_ACCESS_TOKEN = `${import.meta.env.VITE_AIRTABLE_ACCESS_TOKEN}`
+const AIRTABLE_URL = `https://api.airtable.com/v0/${PROPERTY_TABLE_URL}`
 
 const propertyData = async () => {
   try {
-    const res = await fetch(PROPERTY_TABLE_URL, {headers: {
+    const res = await fetch(AIRTABLE_URL, {headers: {
       Authorization: `Bearer ${AIRTABLE_ACCESS_TOKEN}`
     }});
     const data = await res.json()
--- src/services/transactionData.js
+++ src/services/transactionData.js
@@ -1,9 +1,10 @@
 const TRANSACTION_TABLE_URL = `${import.meta.env.VITE_TRANSACTION_TABLE_URL}`
 const AIRTABLE_ACCESS_TOKEN = `${import.meta.env.VITE_AIRTABLE_ACCESS_TOKEN}`
+const AIRTABLE_URL = `https://api.airtable.com/v0/${TRANSACTION_TABLE_URL}`
 
 const transactionData = async () => {
   try {
-    const res = await fetch(TRANSACTION_TABLE_URL, {headers: {
+    const res = await fetch(AIRTABLE_URL, {headers: {
       Authorization: `Bearer ${AIRTABLE_ACCESS_TOKEN}`
     }});
     const data = await res.json()
